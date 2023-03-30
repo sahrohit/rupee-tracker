0a74bb3a1ee9ca698cae534e21a43408ee6a9ab0
Rohit Sah <sahrohit9586@gmail.com>
2023-03-30 23:41:43 +0200

    Added more Nepali Vowels

--- src/data/prefix.ts
+++ src/data/prefix.ts
@@ -1,4 +1,16 @@
 export const NOTE_PREFIX = [
+	{ ro_name: "A", de_name: "अ", value: "A - अ" },
+	{ ro_name: "AA", de_name: "आ", value: "AA - आ" },
+	{ ro_name: "E", de_name: "इ", value: "E - इ" },
+	{ ro_name: "EE", de_name: "ई", value: "EE - ई" },
+	{ ro_name: "O", de_name: "उ", value: "O - उ" },
+	{ ro_name: "OO", de_name: "ऊ", value: "OO - ऊ" },
+	{ ro_name: "AI", de_name: "ए", value: "AI - ए" },
+	{ ro_name: "AII", de_name: "ऐ", value: "AII - ऐ" },
+	{ ro_name: "OH", de_name: "ओ", value: "OH - ओ" },
+	{ ro_name: "AU", de_name: "औ", value: "AU - औ" },
+	{ ro_name: "AM", de_name: "अं", value: "AU - अं" },
+	{ ro_name: "AH", de_name: "अः", value: "AU - अः" },
 	{ ro_name: "KA", de_name: "क", value: "KA - क" },
 	{ ro_name: "KHA", de_name: "ख", value: "KHA - ख" },
 	{ ro_name: "GA", de_name: "ग", value: "GA - ग" },
@@ -13,7 +25,6 @@ export const NOTE_PREFIX = [
 	{ ro_name: "THAA", de_name: "ठ", value: "THA - ठ" },
 	{ ro_name: "DAA", de_name: "ड", value: "DA - ड" },
 	{ ro_name: "DHAA", de_name: "ढ", value: "DHA - ढ" },
-	{ ro_name: "NAA", de_name: "ण", value: "DHA - ण" },
 	{ ro_name: "NAA", de_name: "ण", value: "DHA - ण" },
 	{ ro_name: "TA", de_name: "त", value: "TA - त" },
 	{ ro_name: "THA", de_name: "थ", value: "THA - थ" },
@@ -37,3 +48,18 @@ export const NOTE_PREFIX = [
 	{ ro_name: "TRA", de_name: "त्र", value: "TRA - त्र" },
 	{ ro_name: "GYA", de_name: "ज्ञ", value: "GYA - ज्ञ" },
 ];
+
+export const get_de_name = (ro_name: string) => {
+	const prefix = NOTE_PREFIX.find((item) => item.ro_name === ro_name);
+	return prefix?.de_name;
+};
+
+export const get_ro_name = (de_name: string) => {
+	const prefix = NOTE_PREFIX.find((item) => item.de_name === de_name);
+	return prefix?.ro_name;
+};
+
+export const get_prefix = (value: string) => {
+	const prefix = NOTE_PREFIX.find((item) => item.value === value);
+	return { ro_name: prefix?.ro_name, de_name: prefix?.de_name};
+}
